579bd1d85c0db55bca4b1009073bdc04186ee295
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-04-18 15:35:23 -0700

    scraped data sent to mongo database

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
+var express = require("express");
 var cheerio = require("cheerio");
-// var mongojs = require("mongojs");
+var mongojs = require("mongojs");
 var axios = require("axios");
 
 console.log("\n********************************\n" +
             "Scraping Data from SITENAME\n" +
             "\n********************************\n"
 );
+
+//Initialize express
+var app = express();
+
+//Database configuration
+var databaseURL = "scraped";
+var collections = ["scrapedData"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseURL, collections);
+
 
 //makes axios request for desired site
 
@@ -22,17 +34,27 @@ $("div.post-wrapper").each(function(i,element){
 
     var link = $(element).find("h1").children().attr("href");
 
-    
     var summary = $(element).text();
-    results.push({
 
-        title:title,
-        link:link,
-        summary:summary
-        
-
+    //if it contains title,link, and summary, send to db
+    if(title && link && summary){
+    
+    //insert the data into scrapedData db
+        db.scrapedData.insert({
+            title:title,
+            link:link,
+            summary:summary
+        },
+        function(err, inserted){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(inserted);
+            }
         });
-        console.log(results);
+    }
+        
 
     });
 
