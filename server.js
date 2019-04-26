cc590839c7b2fc42dbbf92d4de0a26cb0f524353
bdiddy <44068902+bmdito@users.noreply.github.com>
2019-04-25 21:33:10 -0700

    one more fix

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ console.log("\n********************************\n" +
 //require models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
+
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraped";
 
